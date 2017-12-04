0e0e7b43e00cc979c1c59ea69900750e0b76beb6
undervoid <yangqifan02@hotmail.com>
2017-12-04 22:01:05 +0100

    Initial commit

---
+++ app/src/main/java/com/bestpratice/qifan/uipracticeone/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bestpratice.qifan.uipracticeone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
