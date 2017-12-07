d601477ce294dcf02993f3d83ee6be980829fa35
undervoid <yangqifan02@hotmail.com>
2017-12-07 21:30:39 +0100

    init project

---
+++ app/src/main/java/com/bestpratice/qifan/uipracticeone/practice/Practice11PieChartView.java
@@ -0,0 +1,33 @@
+package com.bestpratice.qifan.uipracticeone.practice;
+
+import android.view.View;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+/**
+ * Created by user on 2017/12/4.
+ */
+
+public class Practice11PieChartView extends View {
+    public Practice11PieChartView(Context context) {
+        super(context);
+    }
+
+    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+//        综合练习
+//        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+    }
+}
