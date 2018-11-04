2e95b84d92f8f6fc9a88d27545f3dd5000920da8
underwindfall <yangqifan02@hotmail.com>
2018-11-04 15:46:07 +0100

    add solution

--- view2_1/app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
+++ view2_1/app/src/main/java/com/hencoder/hencoderpracticelayout1/practice/Practice01SquareImageView.java
@@ -27,11 +27,17 @@ public class Practice01SquareImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
         // 先用 getMeasuredWidth() 和 getMeasuredHeight() 取到 super.onMeasure() 的计算结果
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
 
         // 然后通过计算，让宽度和高度一致
-
+        if (measuredWidth > measuredHeight) {
+            measuredWidth = measuredHeight;
+        } else {
+            measuredHeight = measuredWidth;
+        }
         // 再用 setMeasuredDimension(width, height) 来保存最终的宽度和高度
+        setMeasuredDimension(measuredWidth, measuredHeight);
     }
 }
